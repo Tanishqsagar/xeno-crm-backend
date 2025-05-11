946778249adda2d37918c5bd4f92da67a7d022d8
Tanishqsagar <tanishqsagar28@gmail.com>
2025-05-11 16:11:23 +0530

    Add frontend URL to CORS config

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(passport.session());
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['https://xeno-crm-frontend-three.vercel.app/', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
