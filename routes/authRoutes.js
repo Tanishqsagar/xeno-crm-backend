eb8e0bfb81913184db35332e92d28c90cb88683d
Tanishqsagar <tanishqsagar28@gmail.com>
2025-05-11 16:17:41 +0530

    Frontend Url added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import passport from 'passport';
 
 const router = express.Router();
+const VERCEL_URL = process.env.VERCEL_URL || 'http://localhost:5173'
 
 router.get('/google',
   passport.authenticate('google', { scope: ['profile', 'email'] })
@@ -10,14 +11,14 @@ router.get('/google',
 router.get('/google/callback',
   passport.authenticate('google', {
     failureRedirect: '/login',
-    successRedirect: 'http://localhost:5173/create' // or /create
+    successRedirect: `${VERCEL_URL}/create` // or /create
   })
 );
 
 router.get('/logout', (req, res) => {
   req.logout(err => {
     if (err) return res.status(500).send('Logout failed');
-    res.redirect('http://localhost:5173');
+    res.redirect(VERCEL_URL);
   });
 });
 
