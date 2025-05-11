95c866490145f315f9f80f3d746bece0a87806c2
Tanishqsagar <tanishqsagar28@gmail.com>
2025-05-11 17:24:51 +0530

    changes for app js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(passport.session());
 
 app.use(express.json());
 app.use(cors({
-  origin: ['https://xeno-crm-frontend-three.vercel.app/'],
+  origin: ['https://xeno-crm-frontend-three.vercel.app/','http://localhost:5173',],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
